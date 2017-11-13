ecfdd8f18ef0d5e020890b9eb95e936a70993625
get.it.analysed <get.it.analysed@gmail.com>
2017-11-13 03:43:14 +0100

    My Django Girls app, first commit

---
+++ blogenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\adminuw\desktop\django_p\djangogirls\blogenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
